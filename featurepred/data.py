182f7568e55e5f67e67ce90f28878328ca529256
Carlos G. Gavidia <cgavidiac@gmail.com>
2020-12-06 19:42:24 +0000

    Data accessing code was moved to a Repository Class, including tests

--- featurepred/data.py
+++ featurepred/data.py
@@ -47,6 +47,49 @@ def can_open_image_file(image_path: str):
         return False
 
 
+class BirdDatasetRepository:
+
+    def __init__(self, attributes_data_file: str, images_data_file: str, certainty_id: int):
+        self.attributes_dataframe: DataFrame = pd.read_csv(attributes_data_file, sep='\s+', header=None,
+                                                           error_bad_lines=False,
+                                                           warn_bad_lines=False, usecols=[0, 1, 2, 3],
+                                                           names=[IMAGE_ID_COLUMN, ATTRIBUTE_ID_COLUMN,
+                                                                  IS_PRESENT_COLUMN,
+                                                                  CERTAINTY_ID_COLUMN])
+
+        self.filter_attributes_by_certainty_id(certainty_id=certainty_id)
+        self.attributes_dataframe = self.attributes_dataframe.pivot(index=IMAGE_ID_COLUMN, columns=ATTRIBUTE_ID_COLUMN,
+                                                                    values=IS_PRESENT_COLUMN)
+
+        self.images_dataframe: DataFrame = pd.read_csv(images_data_file, sep='\s+', header=None,
+                                                       error_bad_lines=False,
+                                                       warn_bad_lines=False,
+                                                       index_col=0,
+                                                       usecols=[0, 1],
+                                                       names=[IMAGE_ID_COLUMN, IMAGE_NAME_COLUMN])
+
+    def filter_attributes_by_certainty_id(self, certainty_id: int) -> None:
+        self.attributes_dataframe = self.attributes_dataframe.loc[
+            self.attributes_dataframe[CERTAINTY_ID_COLUMN] == certainty_id]
+
+    def get_images_by_attribute_value(self, attribute_id: int, attribute_value: int) -> DataFrame:
+        matching_images_dataframe: DataFrame = self.attributes_dataframe.loc[
+            self.attributes_dataframe[attribute_id] == attribute_value]
+
+        logging.info("Images with value {} for attribute {}: {}".format(attribute_value, attribute_id,
+                                                                        len(matching_images_dataframe.index)))
+
+        return matching_images_dataframe
+
+    def get_name_from_image_id(self, image_id) -> str:
+        if image_id in self.images_dataframe.index:
+            image_name: str = self.images_dataframe.loc[image_id][IMAGE_NAME_COLUMN]
+            return image_name
+
+        logging.error("Image Id {} not found".format(image_id))
+        return ''
+
+
 class ImageFolderBuilder:
 
     def __init__(self, attributes_data_file: str, images_data_file: str, image_directory: str):
@@ -55,28 +98,8 @@ class ImageFolderBuilder:
         self.images_data_file = images_data_file
 
     def build(self, attribute_id: int, certainty_id: int, positive_image_folder: str, negative_image_folder: str):
-        attributes_dataframe: DataFrame = pd.read_csv(self.attributes_data_file, sep='\s+', header=None,
-                                                      error_bad_lines=False,
-                                                      warn_bad_lines=False, usecols=[0, 1, 2, 3],
-                                                      names=[IMAGE_ID_COLUMN, ATTRIBUTE_ID_COLUMN, IS_PRESENT_COLUMN,
-                                                             CERTAINTY_ID_COLUMN])
-
-        attributes_dataframe = attributes_dataframe.loc[attributes_dataframe[CERTAINTY_ID_COLUMN] == certainty_id]
-
-        attributes_dataframe = attributes_dataframe.pivot(index=IMAGE_ID_COLUMN, columns=ATTRIBUTE_ID_COLUMN,
-                                                          values=IS_PRESENT_COLUMN)
-
-        images_dataframe: DataFrame = pd.read_csv(self.images_data_file, sep='\s+', header=None,
-                                                  error_bad_lines=False,
-                                                  warn_bad_lines=False,
-                                                  index_col=0,
-                                                  usecols=[0, 1],
-                                                  names=[IMAGE_ID_COLUMN, IMAGE_NAME_COLUMN])
-
         self.copy_by_attribute_value(attribute_id=attribute_id, attribute_value=PRESENT_ATTRIBUTE_VALUE,
-                                     target_directory=positive_image_folder,
-                                     attributes_dataframe=attributes_dataframe,
-                                     images_dataframe=images_dataframe)
+                                     target_directory=positive_image_folder, certainty_id=certainty_id)
 
         # self.copy_by_attribute_value(attribute_id=attribute_id, attribute_value=ABSENT_ATTRIBUTE_VALUE,
         #                              target_directory=negative_image_folder,
@@ -84,23 +107,18 @@ class ImageFolderBuilder:
         #                              images_dataframe=images_dataframe)
 
     def copy_by_attribute_value(self, attribute_id: int, attribute_value: int, target_directory: str,
-                                attributes_dataframe: DataFrame,
-                                images_dataframe: DataFrame):
+                                certainty_id: int):
+        bird_repository: BirdDatasetRepository = BirdDatasetRepository(attributes_data_file=self.attributes_data_file,
+                                                                       images_data_file=self.images_data_file,
+                                                                       certainty_id=certainty_id)
 
-        matching_images_dataframe: DataFrame = attributes_dataframe.loc[
-            attributes_dataframe[attribute_id] == attribute_value]
+        images_dataframe: DataFrame = bird_repository.get_images_by_attribute_value(attribute_id=attribute_id,
+                                                                                    attribute_value=attribute_value)
 
-        logging.info("Images with value {} for attribute {}: {}".format(attribute_value, attribute_id,
-                                                                        len(matching_images_dataframe.index)))
-
-        for index, image_row in matching_images_dataframe.iterrows():
-            image_id = index
-            if image_id in images_dataframe.index:
-                image_name: str = images_dataframe.iloc[image_id][IMAGE_NAME_COLUMN]
-                original_file: str = self.image_directory + image_name
-                copy_file(original_file=original_file, destination_folder=target_directory)
-            else:
-                logging.error("Image Id {} not found in {}".format(image_id, self.images_data_file))
+        for index, image_row in images_dataframe.iterrows():
+            image_name: str = bird_repository.get_name_from_image_id(image_id=index)
+            original_file: str = self.image_directory + image_name
+            copy_file(original_file=original_file, destination_folder=target_directory)
 
 
 def copy_file(original_file: str, destination_folder: str):
---
+++ tests/test_data.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from pandas import DataFrame
+
+from featurepred.data import BirdDatasetRepository
+
+
+class TestDataUtils(TestCase):
+
+    def test_bird_dataset_repository(self):
+        attributes_data_file: str = 'image_attribute_labels_test.txt'
+        certainty_id: int = 4
+        images_data_file: str = 'images_test.txt'
+        bird_repository: BirdDatasetRepository = BirdDatasetRepository(attributes_data_file=attributes_data_file,
+                                                                       images_data_file=images_data_file,
+                                                                       certainty_id=certainty_id)
+
+        rows_in_repository: int = len(bird_repository.attributes_dataframe.index)
+        self.assertTrue(rows_in_repository == 2, msg="Rows in repository are {}".format(rows_in_repository))
+
+        attribute_two_value_one: DataFrame = bird_repository.get_images_by_attribute_value(attribute_id=2,
+                                                                                           attribute_value=1)
+        self.assertTrue(len(attribute_two_value_one.index), 1)
+
+        first_image_name: str = bird_repository.get_name_from_image_id(image_id=1)
+        self.assertEqual(first_image_name, "001.Black_footed_Albatross/Black_Footed_Albatross_0046_18.jpg")
+
+        second_image_name: str = bird_repository.get_name_from_image_id(image_id=2)
+        self.assertEqual(second_image_name, "001.Black_footed_Albatross/Black_Footed_Albatross_0009_34.jpg")
